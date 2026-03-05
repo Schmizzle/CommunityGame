a330bd7b8fd2d433dfd3fcc23f6d23b66695f358
Schmizzle <adamsch1712@gmail.com>
2026-03-05 12:12:35 +0100

    commented inti tester

--- community-game/Environment/TestingStuff/InitTester/init_tester.gd
+++ community-game/Environment/TestingStuff/InitTester/init_tester.gd
@@ -1,10 +1,13 @@
 @tool
 extends Node3D
 
+"""@export var TheMesh: MeshInstance3D
+
+
 @export var Mat: Material:
 	set(value):
 		Mat = value
 		TheMesh.set_surface_override_material(0, value)
 		return value
 
-@export var TheMesh: MeshInstance3D
+"""
