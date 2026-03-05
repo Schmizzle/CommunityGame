e90c24e5c028a3569d0cc2cfc638c5b8bf0f8be2
Schmizzle <adamsch1712@gmail.com>
2026-03-05 10:22:31 +0100

    add blender pipeline and rakugo addon

---
+++ community-game/Addons/Rakugo/Test/TestParser/TestAsk/TestAsk.gd
@@ -0,0 +1,126 @@
+extends GutTest
+
+const Parser = preload("res://addons/Rakugo/lib/systems/Parser.gd")
+
+@onready var parser := Parser.new()
+
+var test_ask_params = [
+	["answer=?\"Hello ?\""],
+	["answer = ? \"Hello ?\""]
+]
+
+func test_ask(params=use_parameters(test_ask_params)):
+	var parsed_script = parser.parse_script(params)
+
+	assert_false(parsed_script.is_empty())
+
+	var parsed_array = parsed_script["parse_array"]
+
+	assert_false(parsed_array.is_empty())
+
+	assert_true(parsed_array[0][0] == "ASK")
+
+	var result = parsed_array[0][1]
+
+	assert_eq(result["variable"], "answer")
+
+	assert_eq(result["question"], "Hello ?")
+
+var test_ask_default_answer_params = [
+	["answer=?\"Hello ?\" \"Bob\""],
+	["answer = ? \"Hello ?\" \"Bob\""]
+]
+
+func test_ask_default_answer(params=use_parameters(test_ask_default_answer_params)):
+	var parsed_script = parser.parse_script(params)
+
+	assert_false(parsed_script.is_empty())
+
+	var parsed_array = parsed_script["parse_array"]
+
+	assert_false(parsed_array.is_empty())
+
+	assert_true(parsed_array[0][0] == "ASK")
+
+	var result = parsed_array[0][1]
+
+	assert_eq(result["variable"], "answer")
+
+	assert_eq(result["question"], "Hello ?")
+
+	assert_eq(result["default_answer"], "Bob")
+
+var test_ask_tag_params = [
+	["answer=?gd \"Hello ?\""],
+	["answer = ? gd \"Hello ?\""]
+]
+
+func test_ask_tag(params=use_parameters(test_ask_tag_params)):
+	var parsed_script = parser.parse_script(params)
+
+	assert_false(parsed_script.is_empty())
+
+	var parsed_array = parsed_script["parse_array"]
+
+	assert_false(parsed_array.is_empty())
+
+	assert_true(parsed_array[0][0] == "ASK")
+
+	var result = parsed_array[0][1]
+
+	assert_eq(result["variable"], "answer")
+
+	assert_eq(result["character_tag"], "gd")
+
+	assert_eq(result["question"], "Hello ?")
+
+var test_ask_all_params = [
+	["answer=?gd \"Hello ?\" \"Bob\""],
+	["answer = ? gd \"Hello ?\" \"Bob\""]
+]
+
+func test_ask_all(params=use_parameters(test_ask_all_params)):
+	var parsed_script = parser.parse_script(params)
+
+	assert_false(parsed_script.is_empty())
+
+	var parsed_array = parsed_script["parse_array"]
+
+	assert_false(parsed_array.is_empty())
+
+	assert_true(parsed_array[0][0] == "ASK")
+
+	var result = parsed_array[0][1]
+
+	assert_eq(result["variable"], "answer")
+
+	assert_eq(result["character_tag"], "gd")
+
+	assert_eq(result["question"], "Hello ?")
+
+	assert_eq(result["default_answer"], "Bob")
+
+func test_ask_quotes():
+	var rk_script = [
+		"answer=?gd \"Hello: \"quote\"?\" \"Bob: \"quote\"\""
+	]
+
+	var parsed_script = parser.parse_script(rk_script)
+
+	assert_false(parsed_script.is_empty())
+
+	var parsed_array = parsed_script["parse_array"]
+
+	assert_false(parsed_array.is_empty())
+
+	assert_true(parsed_array[0][0] == "ASK")
+
+	var result = parsed_array[0][1]
+
+	assert_eq(result["variable"], "answer")
+
+	assert_eq(result["character_tag"], "gd")
+
+	assert_eq(result["question"], "Hello: \"quote\"?")
+
+	assert_eq(result["default_answer"], "Bob: \"quote\"")
---
+++ community-game/Addons/Rakugo/Test/TestParser/TestVariables/TestVariables.gd
@@ -0,0 +1,47 @@
+extends "res://Test/RakugoTest.gd"
+
+func test_variables():
+	var file_path = "res://Test/TestParser/TestVariables/TestVariables.rk"
+
+	var file_base_name = get_file_base_name(file_path)
+
+	watch_rakugo_signals()
+
+	await wait_parse_and_execute_script(file_path)
+
+	var error_str = "Executer::do_execute_script::SET_VARIABLE, Cannot resolve assignement: f of type(2) += with type(4)"
+
+	await wait_execute_script_finished(file_base_name, error_str)
+
+	assert_variable("a", TYPE_INT, 1)
+
+	assert_variable("b", TYPE_FLOAT, 3.5)
+
+	assert_variable("c", TYPE_STRING, "Hello, world !")
+
+	assert_variable("d", TYPE_INT, -1)
+
+	assert_variable("Sy.name", TYPE_STRING, "Sylvie")
+	
+	assert_variable("e", TYPE_INT, Rakugo.get_variable("Sy.life"))
+	
+	assert_variable("Sy.life", TYPE_INT, 10)
+
+	assert_variable("boolean", TYPE_BOOL, true)
+	
+	assert_variable("boolean", TYPE_BOOL, false)
+
+func test_variables_in_strings():
+	var file_path = "res://Test/TestParser/TestVariables/TestDynamicVars.rk"
+
+	var file_base_name = get_file_base_name(file_path)
+
+	watch_rakugo_signals()
+
+	await wait_parse_and_execute_script(file_path)
+
+	assert_variable("test_var", TYPE_STRING, "c")
+
+	assert_variable("test_var_b", TYPE_STRING, "cb")
+
+	await wait_execute_script_finished(file_base_name)
