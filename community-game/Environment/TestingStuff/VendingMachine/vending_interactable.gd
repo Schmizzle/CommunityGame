8f2214cb9e388c13b56b4f2906de1ac79d7c1153
Tural <turcraft@gmail.com>
2026-03-05 10:56:42 +0100

    Added SodaCans

--- community-game/Environment/TestingStuff/VendingMachine/vending_interactable.gd
+++ community-game/Environment/TestingStuff/VendingMachine/vending_interactable.gd
@@ -1,7 +1,12 @@
 extends StaticBody3D
 
 @export var Message: String = "Vended!"
-
+@export var sodaCan: PackedScene
 
 func _on_interactable_component__interaction_signal() -> void:
 	print(Message)
+  	# Creates a new instance of the _spawn_scene
+	var spawn = preload("res://Environment/SodaCan/soda_can.tscn").instantiate() 
+
+	$SpawnLoc.add_child(spawn)
+	$SpawnLoc.global_position = spawn.global_position
