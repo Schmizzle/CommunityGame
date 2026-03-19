aaa09b7bade4e7dec7cb99ee84a621e05b5b54ae
Schmizzle <adamsch1712@gmail.com>
2026-03-19 10:00:43 +0100

    move things around

--- community-game/Environment/TestingStuff/VendingMachine/vending_interactable.gd
+++ community-game/Environment/TestingStuff/VendingMachine/vending_interactable.gd
@@ -6,7 +6,7 @@ extends StaticBody3D
 func _on_interactable_component__interaction_signal() -> void:
 	print(Message)
   	# Creates a new instance of the _spawn_scene
-	var spawn = preload("res://Environment/SodaCan/soda_can.tscn").instantiate() 
+	var spawn = preload("res://Environment/TestingStuff/VendingMachine/SodaCan/soda_can.tscn").instantiate() 
 
 	$SpawnLoc.add_child(spawn)
 	$SpawnLoc.global_position = spawn.global_position
