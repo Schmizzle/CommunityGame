37fad5a5ae01a3a5043ba1d5241c29418cf510f1
Schmizzle <adamsch1712@gmail.com>
2026-03-20 16:24:31 +0100

    remove dead showcase code

--- community-game/Environment/Elements/General/InteractableComponent/interactable_component.gd
+++ community-game/Environment/Elements/General/InteractableComponent/interactable_component.gd
@@ -7,5 +7,3 @@ signal _interaction_signal
 func on_interacted() -> void:
 	_interaction_signal.emit()
 	
-	
-	Globals.PlayerReference.QuestManagerNode.try_increment_task(ProgressionTracker.TaskTags.Test_SodaDrink)
